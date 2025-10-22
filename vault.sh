cd99ec5ec39d1f2e7f1f1d2de11c3156e2632661
ph4 <pahn1337@gmail.com>
2025-10-22 17:40:38 +0300

    FreeRADIUS sql auth

--- vault.sh
+++ vault.sh
@@ -50,7 +50,7 @@ run() {
     ansible-vault view --vault-id "$VAULT_NAME@$VAULT_PASS_FILE" inventory/password_check
 
     # Files ending with .vault.yml are encrypted
-    find -name "*.vault.yml" | while read -r file; do
+    find -name "*vault.yml" | while read -r file; do
         echo "$file"
         if [[ -f "$file" ]]; then
             "$1" "$file"
